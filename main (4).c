1f60c9793b49476bb7374c09d03853668df8f616
ilaedna16 <115794672+ilaedna16@users.noreply.github.com>
2022-11-16 16:48:00 -0300

    Add files via upload

---
+++ main (4).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+   int n,c; 
+   
+   printf ("digite um numero"); 
+   scanf ("%d", &n); 
+   
+   
+   n= (n*n*n); 
+   
+   printf ("o valor do cubo eh: %d\n", n); 
+    
+    
+    return 0;
+}
